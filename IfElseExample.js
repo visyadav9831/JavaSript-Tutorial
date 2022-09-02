455710c2ee31328c729021e659a4ba3cec1e254c
jeetyadu <visyadav9005@gmail.com>
2022-09-02 22:35:04 +0530

    DEMO-0004: If else example

---
+++ IfElseExample.js
@@ -0,0 +1,4 @@
+var a= 20;
+if(a>10){
+    document.write("Hello World");
+}
---
+++ IfElseTwo.js
@@ -0,0 +1,7 @@
+var a = 20;
+if(a%2 ==0){
+    document.write("Odd Number");
+}
+else{
+    document.write("Even Number");
+}
