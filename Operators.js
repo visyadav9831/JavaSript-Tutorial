ede1b32f298c35b56e8a07a0ad42e3de7ed663d4
jeetyadu <visyadav9005@gmail.com>
2022-09-03 15:10:44 +0530

    DEMO-0006: Operators done

---
+++ Operators.js
@@ -0,0 +1,10 @@
+console.log("Operators")
+let a = 40
+let b = 4
+
+console.log(" a + b =", a+b)
+console.log(" a - b =", a-b)
+console.log(" a * b =", a*b)
+console.log(" a / b =", a/b)
+console.log(" a ** b =", a**b)
+console.log(" a % b =", a%b)
