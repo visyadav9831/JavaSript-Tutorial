c18d9372be2a1882d675dcf03bd3566406e3e5af
jeetyadu <visyadav9005@gmail.com>
2022-09-03 14:42:51 +0530

    DEMO-0005: practiceSetOne is done

---
+++ practiceSetOne.js
@@ -0,0 +1,36 @@
+//Question NUmber One
+
+let a = "Jeet "
+let b = 007
+let c = a+b
+console.log(c);
+
+//Question Number Two == Find Datatype of Question number One
+
+console.log(typeof c)
+
+//Question NUmber Three
+
+const _a = {
+    school_name: "Jeet",
+    standerd: 10,
+    principle: false
+}
+console.log(_a)
+
+_a['home']= "Prayagraj" // adding new property in object
+console.log(_a)
+
+//_a = {}  //we can not assign any number, string etc for new  _a object.
+
+
+//Question number Foure
+
+const dictionary = {
+    ogham: "an alphabetic script used originally for inscriptions in an archaic form of Irish, from about the 5th to the 10th centuries",
+    flokati: "a thick, woolen rug with a shaggy pile, originally handwoven in Greece.",
+    lido: "a fashionable beach resort.",
+    perse: "of a very deep shade of blue or purple.",
+    ailurophile: " a person who likes cats"
+}
+console.log(dictionary.ogham)
