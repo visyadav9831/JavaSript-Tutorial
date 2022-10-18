607029062f3f1808fe0bcb9dcc08cfac2a2eac9a
jeetyadu <visyadav9005@gmail.com>
2022-10-18 22:46:02 +0530

    CHAPTER-0001: Operations HeadToHead etc

---
+++ Operations/Operations.js
@@ -0,0 +1,30 @@
+var a = 20, b=30,c=null;
+
+function add(){
+    c = a+b;
+    document.write(c);
+}
+function subtract(){
+    c = a-b;
+    document.write(c);
+}
+function multi(){
+    c = a*b;
+    document.write(c);
+}
+function div(){
+    c = a/b;
+    document.write(c);
+}
+function modulus(){
+    c = a%b;
+    document.write(c);
+}
+function increment(){
+    a ++;
+    document.write(a);
+}
+function decrement(){
+    a--;
+    document.write(a);
+}
---
+++ javaScriptFile.js
@@ -0,0 +1,13 @@
+var a = 10, b = 20, c = null;
+
+function add(){
+    c = a+b;
+    document.write(c);
+}
+function sub(){
+    c = a-b;
+    document.write(c);
+}
+var name = "Jeet";
+var money;
+money = 2000.50;
