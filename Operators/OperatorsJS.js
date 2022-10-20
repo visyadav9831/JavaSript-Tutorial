1534e1d44db8fcc3586598ca3e58d5eb2de04bfc
jeetyadu <visyadav9005@gmail.com>
2022-10-20 22:16:30 +0530

    CHAPTER-0001: Operators OperatorsJS

---
+++ Operators/OperatorsJS.js
@@ -0,0 +1,27 @@
+var a = 10, b = 20, result = null;
+var linebrake = "</br>"
+
+function EqualTO(){
+    result = (a==b);
+    document.write(result + linebrake);
+}
+function NotEqualTO(){
+    result = (a != b);
+    document.write(result + linebrake);
+}
+function GreaterThan(){
+    result = (a>b);
+    document.write(result + linebrake);
+}
+function LessThan(){
+    result = (a<b);
+    document.write(result + linebrake);
+}
+function GreaterThanOrEqualTO(){
+    result = (a >= b);
+    document.write(result + linebrake);
+}
+function LessThanOrEqualTO(){
+    result = (a <= b);
+    document.write(result + linebrake);
+}
