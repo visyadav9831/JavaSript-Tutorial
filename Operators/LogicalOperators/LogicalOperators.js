57591d7ffc229b28c6279a9e4f6ebd26eceb561b
jeetyadu <visyadav9005@gmail.com>
2022-11-14 23:48:22 +0530

    Squashed commit of the following:
    commit 4db8c3fc785742f60edc2b96dccd90f65b6b73d0
    Author: jeetyadu <visyadav9005@gmail.com>
    Date:   Mon Nov 14 23:45:07 2022 +0530
    
        CHAPTER-0002: Just commit

---
+++ Operators/LogicalOperators/LogicalOperators.js
@@ -0,0 +1,16 @@
+var A=10, B=20, result;
+
+function LogicalAND(){
+    result = A&&B;
+    document.write(result);
+}
+
+function LogicalOR(){
+    result = A||B;
+    document.write(result);
+}
+
+function LOgicalNOT(){
+    result = !(A&&B);
+    document.write(result);
+}
